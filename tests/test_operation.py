b7f64d1e1beb1174a2f43dcf8de99da4f0d799de
mounikaatmakuri <ej66499@umbc.edu>
2025-08-02 21:11:18 -0400

    first commit

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -9,3 +9,24 @@ def test_sub():
     assert sub(4,3)==1
     assert sub(3,3)==0
     assert sub(2,3)==-1
+    
+def test_mul():
+    assert mul(2,3)==6
+    assert mul(-1,1)==-1
+    assert mul(0,5)==0  
+    
+def test_div():
+    assert div(6,3)==2
+    assert div(5,2)==2.5
+    try:
+        div(5,0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero"    
+        
+def test_mod():
+    assert mod(5,3)==2
+    assert mod(5,5)==0
+    try:
+        mod(5,0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero"                    
